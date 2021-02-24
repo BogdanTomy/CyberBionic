8a2de52b0aa8cf751532ca9deb08d843f1d117f7
Dan Tomy <dantomy@Dans-MacBook-Air.local>
2021-02-24 19:41:40 +0200

    homework-lesson-3-part-1

---
+++ directives-homework/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  deleteHandler(id): void {
+    console.log(`Element deleted: id => ${id}`);
+  }
+}
